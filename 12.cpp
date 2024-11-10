207691c4b67163a2390dad9cae60647793bad37a
nikunj <nikunjsanchaniya08@gmail.com>
2024-11-10 15:47:52 +0530

    Add 12.cpp

---
+++ 12.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// A
+// BB
+// CCC
+// DDDD
+
+int main()
+{
+    int r, c, n = 4;
+    char ch = 'A';
+
+    for (r = 1; r <= n; r++)
+    {
+        for (c = 1; c <= r; c++)
+        {
+            cout << ch;
+        }
+        ch++;
+        cout << endl;
+    }
+
+    return 0;
+}
